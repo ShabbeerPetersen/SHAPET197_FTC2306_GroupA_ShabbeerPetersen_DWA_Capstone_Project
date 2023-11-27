262631757d803afe57cc8b42d053002c155d7472
Shabbeer Petersen <shabbeerexilepetersen@gmail.com>
2023-11-27 15:54:59 +0200

    Revert "fixed Login and SignUp links"
    This reverts commit 160eb639517d018218fced6793d3a1a1bab602df.

--- components/Login.jsx
+++ components/Login.jsx
@@ -11,7 +11,7 @@ import Link from "@mui/material/Link";
 import Grid from "@mui/material/Grid";
 import Box from "@mui/material/Box";
 import Typography from "@mui/material/Typography";
-import SignUp from "./Signup";
+import SignUp from "./SignUp";
 
 const Login = (props) => {
   const [email, setEmail] = useState("");
@@ -98,7 +98,7 @@ const Login = (props) => {
               </Link>
             </Grid>
             <Grid item>
-              <Link href="/signup" variant="body2">
+              <Link href= "components/SignUp.jsx" variant="body2">
                 {"Don't have an account? Sign Up"}
               </Link>
             </Grid>
